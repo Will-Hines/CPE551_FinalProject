27efd93342411d49c869872d92529ca89c4387d5
Will-Hines <willhines04@gmail.com>
2026-05-04 15:28:41 -0400

    Finished adding dynamic file and data loading, shifted to returning a dictionary with full appliance objects

--- dataset_loader.py
+++ dataset_loader.py
@@ -4,25 +4,26 @@ Modeule to load the dataset as a pandas dataframe
 
 import pandas as pd
 from pathlib import Path
+from appliance import Appliance
 
 def get_file_names():
     """
     Gets all the file names of all csv files in the /data directory
 
-    Returns: a list containing the names of all files within the /data directory
+    Returns: a list containing Path objects for all files within the /data directory
     """
     folder_path = Path('./data')       # checks the data folder for appliance csv data files
 
     file_names = [f.name for f in folder_path.iterdir() if f.is_file() and f.suffix == '.csv']      # Use Comprehension to put all existing csv files into a list
-    
+
     return file_names
 
-def load_data(file_path : str):
+def load_data(file_path : Path):
     """
     Load the appliance energy data from a csv file
 
     Args:
-        file_path (str): the relative path where the csv can be loacated
+        file_path (Path): a Path object containing the path to the csv file
 
     Returns:
         Pandas dataframe with the open csv file within a file handler
@@ -44,3 +45,23 @@ def load_data(file_path : str):
         raise ValueError("Dataset missing required columns.")
 
     return data
+
+def create_appliance_objects(file_names):
+    """
+    Creates an appliance object for each data file in /data
+
+    Returns: a list of all appliance objects
+    """
+    appliance_dict = {}
+
+    for path in file_names:
+        name = path.stem        # name the appliance after the data file we are loading it from
+        appliance = Appliance(name, load_data(path))        # create the Appliance object with the correct name and use the load_data function to load in the data
+        appliance_dict[name] = appliance            # add the new Appliance object to the dict
+    
+    return appliance_dict
+
+if __name__ == "main":
+    file_names = get_file_names()
+    print(f"Files in /data: {file_names}")
+    print(f"Final appliance dictionary: {create_appliance_objects(file_names)}")
